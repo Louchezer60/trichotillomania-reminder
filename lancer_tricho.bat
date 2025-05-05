435954144b919ab50a93feee027a92fb6e71420b
Louchezer60 <yvanyria@gmail.com>
2025-05-05 22:44:26 +0200

    Update lancer_tricho.bat

--- lancer_tricho.bat
+++ lancer_tricho.bat
@@ -2,4 +2,4 @@
 cd /d "C:\PATH"
 call "%USERPROFILE%\Anaconda3\Scripts\activate.bat" %USERPROFILE%\Anaconda3
 call conda activate "name_of_your_env"
-start cmd /k python tricho-reminder.py
+start cmd /k python main.py
